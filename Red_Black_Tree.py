05abb93bfa36a2d3edbd19fed6437e51b102470c
javacoffer2020 <javacoffer@vip.163.com>
2020-03-09 13:45:09 +0800

    测验上传

---
+++ Red_Black_Tree.py
@@ -0,0 +1,36 @@
+class point:
+
+    def __init__(self, name=0, color=True, box=''):
+        self.name = name
+        self.color = color
+        self.box = box
+
+    def __str__(self):
+        if self.color:
+            c = 'Red'
+        else:
+            c = 'Black'
+        return "Name:{}, color:{}, Box:{}".format(self.name, self.color, self.box)
+
+
+class red_black_tree:
+
+    def __init__(self, point=point(), father=None, lchild=None, rchild=None):
+        self.point = point
+        self.father = father
+        self.lchild = lchild
+        self.rchild = rchild
+
+    def __str__(self):
+        return "Father:{}; Point:{}; lchild:{}; rchild:{}".format(self.father.name, self.lchild.name, self.rchild.name)
+
+
+a = point(1)
+b = point(2)
+c = point(3)
+
+d = red_black_tree()
+d.point = c
+d.lchild = b
+d.rchild = a
+print(d)

4901c81be809fcd0036eb226b9e7b060bdeaf6b0
javacoffer2020 <javacoffer@vip.163.com>
2020-03-18 19:25:55 +0800

    test

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print('hello world')
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+profile = pd.read_csv('E:\Jupyter\GitHub\Transactions.csv')
+print(profile.head())

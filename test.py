8446b266fe981b8ef62fd012b614cebf87c969f7
javacoffer2020 <javacoffer@vip.163.com>
2020-03-18 19:49:30 +0800

    test

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
 
 profile = pd.read_csv('E:\Jupyter\GitHub\Transactions.csv')
 print(profile.head())

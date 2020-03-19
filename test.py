420f5f1a6ace5f90cf35dbc37c174cc809717247
javacoffer2020 <javacoffer@vip.163.com>
2020-03-19 09:23:39 +0800

    200319 0923

--- test.py
+++ test.py
@@ -7,3 +7,5 @@ from sklearn.model_selection import train_test_split
 profile = pd.read_csv('E:\Jupyter\GitHub\Transactions.csv')
 print(profile.head())
 
+train_data, test_data = train_test_split(profile.[],test_size=0.3, train_size=0.7, )
+

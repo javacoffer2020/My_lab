ec5642ba6a9e95d3b78ce3efc24f939484195575
javacoffer2020 <javacoffer@vip.163.com>
2020-04-11 21:16:42 +0800

    200401 2116

---
+++ PythonChalleng/No.2.py
@@ -0,0 +1,16 @@
+str_old = [chr(i) for i in range(97,123)]
+str_new = [chr(i) for i in range(99,123)]
+str_new += ['a', 'b']
+transdict = dict(zip(str_old, str_new))
+print(transdict)
+
+def transword(chr, transdict):
+    
+
+
+
+txt = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+txt_new = ''
+for chr in txt:
+
+

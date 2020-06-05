f06a1577c2c57709277982eada1c390ece701762
javacoffer2020 <javacoffer@vip.163.com>
2020-06-05 11:53:26 +0800

    200605 1153

--- Test_Lab/最小栈.py
+++ Test_Lab/最小栈.py
@@ -8,4 +8,33 @@ class MinStack:
 
     def push(self, element):
         self.main_stack.append(element)
-        # 若辅助栈为空，或
+        # 若辅助栈为空，或新元素的值小于或等于辅助栈栈顶元素的值，
+        # 将新元素压入辅助栈
+        if len(self.min_stack) == 0 or element <= self.min_stack[len(self.min_stack)-1]:
+            self.min_stack.append(element)
+
+    def pop(self):
+        if self.main_stack[len(self.main_stack)-1] == self.min_stack[len(self.min_stack)-1]:
+            self.min_stack.pop()
+        return self.main_stack.pop()
+
+    def get_min(self):
+        if len(self.min_stack) == 0:
+            return None
+        return self.min_stack[len(self.min_stack)-1]
+
+
+my_stack = MinStack()
+my_stack.push(4)
+my_stack.push(9)
+my_stack.push(7)
+my_stack.push(3)
+my_stack.push(8)
+my_stack.push(5)
+print(my_stack)
+print(my_stack.get_min())
+my_stack.pop()
+my_stack.pop()
+my_stack.pop()
+print(my_stack)
+print(my_stack.get_min())

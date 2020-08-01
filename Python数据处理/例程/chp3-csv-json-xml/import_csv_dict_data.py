32c781ea6ee0ba6496b9b71039264f3a1f6121ae
javacoffer2020 <javacoffer@vip.163.com>
2020-08-01 09:18:31 +0800

    200801 0918

--- Python数据处理/例程/chp3-csv-json-xml/import_csv_dict_data.py
+++ Python数据处理/例程/chp3-csv-json-xml/import_csv_dict_data.py
@@ -3,5 +3,10 @@ import csv
 csvfile = open('../../data/chp3/data-text.csv', 'r')
 reader = csv.DictReader(csvfile)
 
+i = 1
 for row in reader:
-    print(row)
+    print("Id No. {:d}".format(i))
+    for key in row:
+        print("{} : {}".format(key, row[key]))
+    print(' ')
+    i += 1
--- Python数据处理/例程/chp3-csv-json-xml/import_xml_data.py
+++ Python数据处理/例程/chp3-csv-json-xml/import_xml_data.py
@@ -2,26 +2,31 @@ from xml.etree import ElementTree as ET
 
 tree = ET.parse('../../data/chp3/data-text.xml')
 root = tree.getroot()
-print root
 
 data = root.find('Data')
+for observation in data:
+    for item in observation:
+        print(item.Dim)
+
 
 all_data = []
-
-for observation in data:
-    record = {}
-    for item in observation:
-
-        lookup_key = item.attrib.keys()[0]
-
-        if lookup_key == 'Numeric':
-            rec_key = 'NUMERIC'
-            rec_value = item.attrib['Numeric']
-        else:
-            rec_key = item.attrib[lookup_key]
-            rec_value = item.attrib['Code']
-
-        record[rec_key] = rec_value
-    all_data.append(record)
-
-print all_data
+# for observation in data:
+#     record = {}
+#     for item in observation:
+#         lookup_key = list(item.attrib.keys())[0]
+#         if lookup_key == 'Numeric':
+#             rec_key = 'NUMERIC'
+#             rec_value = item.attrib['Numeric']
+#         else:
+#             rec_key = item.attrib[lookup_key]
+#             rec_value = item.attrib['Code']
+#         record[rec_key] = rec_value
+#     all_data.append(record)
+#
+# i = 1
+# for data in all_data:
+#     print("Id No. {}".format(i))
+#     for item in data:
+#         print("    {} : {}".format(item, data[item]))
+#     print(" ")
+#     i += 1

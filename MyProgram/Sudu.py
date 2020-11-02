0ac1ef64a6d2810bd916be633a7b2ed07ac2a817
javacoffer2020 <javacoffer@vip.163.com>
2020-11-02 20:42:13 +0800

    201102 2042

--- MyProgram/Sudu.py
+++ MyProgram/Sudu.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 
+a = pd.DataFrame({"A": [1, 2, 3, 4], "B": [5, 6, 7, 8]})
+print(a)
+
+b = pd.DataFrame({"C": ['a', 'b', 'c', 'd'], "B": ['e', 'f', 'g', 'h']})
+
+print(b)
+
+c = pd.concat([a,b],axis=1)
+print(c)
